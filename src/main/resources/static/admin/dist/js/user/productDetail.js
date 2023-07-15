f27311a5484b0ec30ab84a856aa74940564627f6
nhatdan2003 <dantran2910@gmail.com>
2023-07-16 01:15:00 +0700

    dantn

---
+++ src/main/resources/static/admin/dist/js/user/productDetail.js
@@ -0,0 +1,57 @@
+
+  $(document).ready(function () {
+    $('.slider-for').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        fade: true,
+        asNavFor: '.image-slider'
+      });
+    });
+
+  $(document).ready(function () {
+    $('.image-slider').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        asNavFor: '.slider-for',
+        prevArrow: `<button type='button' class='slick-prev slick-arrow'><i class="fa-solid fa-chevron-left"></i></button>`,
+        nextArrow: `<button type='button' class='slick-next slick-arrow'><i class="fa-solid fa-chevron-right"></i></button>`,
+        // dots: true,
+        centerMode: true,
+        focusOnSelect: true,
+        // infinite: false,
+      });
+    });
+
+    $(document).ready(function () {
+      $(".card-slider").slick({
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        // autoplay: true,
+        autoplaySpeed: 3000,
+        infinite: true,
+        arrows: true,
+        draggable: true,
+        prevArrow: `<button type='button' class='slick-prev slick-arrow'><i class="fa-solid fa-chevron-left"></i></button>`,
+        nextArrow: `<button type='button' class='slick-next slick-arrow'><i class="fa-solid fa-chevron-right"></i></button>`,
+      //   dots: true,
+        responsive: [
+          {
+            breakpoint: 1025,
+            settings: {
+              slidesToShow: 3,
+            },
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              arrows: false,
+              infinite: false,
+            },
+          },
+        ],
+       
+      });
+    });
+ 
