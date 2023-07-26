748c1784ee77a6eb67abda891c44477059617fe6
nhatdan2003 <dantran2910@gmail.com>
2023-07-26 23:18:55 +0700

    mobiledetails

--- src/main/java/com/Devex/Controller/ProductDetailController.java
+++ src/main/java/com/Devex/Controller/ProductDetailController.java
@@ -1,6 +1,9 @@
 package com.Devex.Controller;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,19 +24,42 @@ public class ProductDetailController {
 
 	@Autowired
 	ProductService productService;
-	
+
 	@Autowired
 	ProductVariantService productVariantService;
-	
+
 	@RequestMapping("/details/{id}")
 	public String details(ModelMap model, @PathVariable("id") String id) {
-		System.out.println("ssssssssss"+id);
+//		System.out.println("ssssssssss"+id);
 		Product product = productService.findById(id).orElse(new Product()); // or any other default value
+//		Map<String, Integer> mapSize = new HashMap<>();
+//		Map<String, Integer> mapColor = new HashMap<>();
+		//list Size
+		List<String> listSize = new ArrayList<>();
+
+		product.getProductVariants().forEach(sv -> {
+			if (!listSize.contains(sv.getSize())) {
+				listSize.add(sv.getSize());
+				System.out.println(sv.getSize());
+			}
+		});
 		
-		System.out.println(product.getId());
+		
+		//ListColor
+		List<String> listColor = new ArrayList<>();
+
+		product.getProductVariants().forEach(sv -> {
+			if (!listColor.contains(sv.getColor())) {
+				listColor.add(sv.getColor());
+				System.out.println(sv.getColor());
+			}
+		});
+		
+		model.addAttribute("listColor", listColor);
+		model.addAttribute("listSize", listSize);
 		model.addAttribute("prodt", product);
+
 		return "user/productDetail";
 	}
-	
-	
+
 }
--- src/main/resources/static/admin/dist/js/user/productDetail.js
+++ src/main/resources/static/admin/dist/js/user/productDetail.js
@@ -25,16 +25,16 @@
 
     $(document).ready(function () {
       $(".card-slider").slick({
-        slidesToShow: 4,
-        slidesToScroll: 4,
-        // autoplay: true,
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        autoplay: true,
         autoplaySpeed: 3000,
         infinite: true,
         arrows: true,
         draggable: true,
         prevArrow: `<button type='button' class='slick-prev slick-arrow'><i class="fa-solid fa-chevron-left"></i></button>`,
         nextArrow: `<button type='button' class='slick-next slick-arrow'><i class="fa-solid fa-chevron-right"></i></button>`,
-      //   dots: true,
+     
         responsive: [
           {
             breakpoint: 1025,
