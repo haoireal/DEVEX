f39dbb1593136d290619ae2eef95ba8d729910e3
baolh106 <baolh106@gmail.com>
2023-08-05 03:08:02 +0700

    chinh sua chut xiu

--- src/main/java/com/Devex/Controller/customer/AccountController.java
+++ src/main/java/com/Devex/Controller/customer/AccountController.java
@@ -1,6 +1,9 @@
 package com.Devex.Controller.customer;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -65,6 +68,10 @@ public class AccountController {
 	
 	@Autowired
 	ParamService param;
+	
+	@Autowired
+	PasswordEncoder passwordEncoder;
+	
 	
 	@GetMapping("/signup")
 	public String showSignup() {
@@ -133,7 +140,7 @@ public class AccountController {
 		user.setFullname(fullname);
 		user.setPhone(phone);
 		user.setEmail(email);
-		user.setPassword(password);
+		user.setPassword(passwordEncoder.encode(password));
 		user.setCreateDay(createdDate);
 		user.setGender(gender);
 		user.setActive(active);
