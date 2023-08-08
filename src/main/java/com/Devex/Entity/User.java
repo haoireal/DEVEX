b6b3aa49ff3716d24f3769024f8d404962b31e6d
puqqe _haoireal <kenipham147@gmail.com>
2023-08-09 00:11:42 +0700

    code chinh

--- src/main/java/com/Devex/Entity/User.java
+++ src/main/java/com/Devex/Entity/User.java
@@ -14,10 +14,7 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Inheritance;
 import jakarta.persistence.InheritanceType;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
-import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import jakarta.persistence.Temporal;
 import jakarta.persistence.TemporalType;
@@ -60,7 +57,6 @@ public class User implements Serializable{
 	@Column(name = "Active")
 	private Boolean active;
 	
-	@JsonIgnore
 	@OneToMany(mappedBy = "user", fetch = FetchType.EAGER)
 	@JsonIgnore
 	private List<UserRole> roles;
