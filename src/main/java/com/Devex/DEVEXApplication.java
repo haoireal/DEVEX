3f1dcd30114409da1958445143c3c349a6b56a1c
nhatdan2003 <dantran2910@gmail.com>
2023-11-26 12:28:42 +0700

    fixIndex

---
+++ src/main/java/com/Devex/DEVEXApplication.java
@@ -0,0 +1,24 @@
+package com.Devex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+
+@SpringBootApplication
+@EnableConfigurationProperties
+public class DEVEXApplication {
+	/*
+	@Autowired
+	private TwilioConfig twilioConfig;
+	
+	@PostConstruct
+	public void setup() {
+		Twilio.init(twilioConfig.getAccountSid(), twilioConfig.getAuthToken());
+	}
+	*/
+	public static void main(String[] args) {
+		SpringApplication.run(DEVEXApplication.class, args);
+	}
+
+}
